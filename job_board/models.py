7bc639303d5ab933a72b1b1ca7518cda89c0a5f1
abhiabhi94 <13880786+abhiabhi94@users.noreply.github.com>
2025-03-24 18:49:25 +0530

    Prettify string representation of Job
    - also use a jinja template for e-mail

--- job_board/models.py
+++ job_board/models.py
@@ -1,16 +1,20 @@
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Column, Integer, String, Numeric, DateTime
 from sqlalchemy import Boolean
-import humanize
-import markdown
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy import select, update
+from jinja2 import Environment, FileSystemLoader
+import pathlib
 
 from job_board.logger import logger
 from job_board.connection import Session, engine
 from job_board.notifier.mail import EmailProvider
 from job_board import config
 from job_board.base import Job as JobListing
+
+jinja_env = Environment(
+    loader=FileSystemLoader(pathlib.Path(__file__).parent / "templates"),
+)
 
 
 # Base class for all models
@@ -32,14 +36,6 @@ class Job(Base):
     salary = Column(Numeric, nullable=False)
     posted_on = Column(DateTime)
     notified = Column(Boolean, default=False)
-
-    def __repr__(self) -> str:
-        return markdown.markdown(f"""
-        ### Title: {self.title}
-        **Salary: ${self.salary:,}**
-        Link: {self.link}
-        Posted: {humanize.naturaltime(self.posted_on)}
-        """)
 
 
 def store_jobs(jobs: JobListing):
@@ -72,13 +68,28 @@ def store_jobs(jobs: JobListing):
 def notify():
     job_ids = []
     statement = select(Job).where(Job.notified.is_(False))
+    template = jinja_env.get_template("mail.html")
+    subject = "Jobs To Apply"
+
     with Session.begin() as session:
         jobs = session.execute(statement).scalars().all()
         job_ids = [job.id for job in jobs]
-        email_body = "\n\n".join([str(job) for job in jobs])
+        job_listings = [
+            JobListing(
+                link=job.link,
+                title=job.title,
+                salary=job.salary,
+                posted_on=job.posted_on,
+            )
+            for job in jobs
+        ]
+
+    email_body = template.render(
+        jobs=job_listings,
+        subject=subject,
+    )
 
     logger.debug(f"Number of jobs to send:::{len(job_ids)}")
-
     if not len(job_ids):
         logger.info("No new jobs found for notification!")
         return
@@ -86,14 +97,13 @@ def notify():
     # TODO - send email in batches..
     # add constraints in number of jobs to send in one mail
     email_provider = EmailProvider()
-    email_subject = "Jobs To Apply"
 
-    logger.debug(f"Email to send:::{email_body}")
+    logger.debug(f"Email to send:::\n{email_body}")
 
     email_provider.send_email(
         sender=config.SERVER_EMAIL,
         receivers=config.RECEIPIENTS,
-        subject=email_subject,
+        subject=subject,
         body=email_body,
     )
 
