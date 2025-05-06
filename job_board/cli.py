abhiabhi94/job-board

commit e14650e8dc618088c76504138a85ef2368520b1e

Use explicit read write or readonly sessions
- the earlier approach always used readwrite sessions.

The testing part currently sucks, but don't have a better
approach right now.

job_board/cli.py
@@ -105,30 +105,25 @@ def _fetch(
     for portal_name, portal_class in PORTALS.items():
         if portal_name.lower() in portals:
             click.echo(f"Fetching jobs from {portal_name.title()}")
-            session = get_session()
-            with session.begin():
-                setting = (
-                    session.query(PortalSetting)
-                    .filter(PortalSetting.portal_name == portal_name)
-                    .scalar()
-                )
-                setting_id = setting.id
-                last_run_at = None
-                if setting.last_run_at:
-                    last_run_at = setting.last_run_at.astimezone(timezone.utc)
-                    # just to have a buffer.
-                    last_run_at -= timedelta(minutes=5)
+            setting = PortalSetting.get_or_create(portal_name=portal_name)
+            setting_id = setting.id
+            last_run_at = None
+            if setting.last_run_at:
+                last_run_at = setting.last_run_at.astimezone(timezone.utc)
+                # just to have a buffer.
+                last_run_at -= timedelta(minutes=5)
 
             jobs = portal_class(last_run_at=last_run_at).get_jobs()
             logger.debug(f"Jobs from {portal_name}:\n\n{jobs}")
             store_jobs(jobs)
             all_jobs.extend(jobs)
 
-            session = get_session()
-            with session.begin():
+            session = get_session(readonly=False)
+            with session:
                 setting = session.get(PortalSetting, setting_id)
                 setting.last_run_at = datetime.now(timezone.utc)
                 session.add(setting)
+                session.flush()
 
     if to_notify:
         notify()

job_board/connection.py
@@ -1,10 +1,11 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
 from job_board import config
 
 _engine = None
 _SessionFactory = None
+_test_session = None
 
 
 def get_engine():
@@ -14,16 +15,29 @@ def get_engine():
             config.DATABASE_URL,
             echo=config.SQL_DEBUG,
         )
+
     return _engine
 
 
 def _get_session_factory():
     global _SessionFactory
     if _SessionFactory is None:
-        _SessionFactory = sessionmaker(bind=get_engine())
+        _SessionFactory = sessionmaker(
+            bind=get_engine(),
+            future=True,
+        )
     return _SessionFactory
 
 
-def get_session():
+def get_session(*, readonly=True):
+    global _test_session
+    if _test_session:
+        if not config.TEST_ENV:
+            raise RuntimeError("Please use the test environment to run tests. ")
+        return _test_session
+
     Session = _get_session_factory()
-    return Session()
+    session = Session()
+    if readonly:
+        session.execute(text("SET TRANSACTION READ ONLY"))
+    return session

job_board/models.py
@@ -63,8 +63,8 @@ def store_jobs(jobs: JobListing):
         values.append(value)
 
     insert_statement = insert(Job).values(values).returning(Job.id)
-    session = get_session()
-    with session.begin():
+    session = get_session(readonly=False)
+    with session:
         statement = insert_statement.on_conflict_do_nothing(
             index_elements=[
                 func.lower(Job.link),
@@ -81,8 +81,8 @@ def notify():
         .where(Job.notified.is_(False))
         .order_by(Job.salary.desc(), Job.posted_on.desc())
     )
-    session = get_session()
-    with session.begin():
+    session = get_session(readonly=True)
+    with session:
         jobs = session.execute(statement).scalars().all()
         if not jobs:
             logger.info("No new jobs found for notification")
@@ -126,6 +126,6 @@ def notify():
             .where(Job.notified.is_(False), Job.id.in_(batched_ids))
             .values(notified=True)
         )
-        session = get_session()
-        with session.begin():
+        session = get_session(readonly=False)
+        with session:
             session.execute(statement)

job_board/portals/models.py
@@ -22,9 +22,21 @@ class PortalSetting(BaseModel):
     )
 
     @classmethod
-    def create_portal_setting(cls):
-        session = get_session()
-        with session.begin():
-            existing_settings = session.query(cls).all()
-            portals = set(PORTALS.keys()) - {s.portal_name for s in existing_settings}
-            session.add_all(cls(portal_name=p) for p in portals)
+    def get_or_create(cls, portal_name: str) -> "PortalSetting":
+        if portal_name not in PORTALS:
+            raise ValueError(f"Portal {portal_name} is not supported")
+
+        session = get_session(readonly=False)
+        with session:
+            setting = (
+                session.query(PortalSetting)
+                .filter(PortalSetting.portal_name == portal_name)
+                .one_or_none()
+            )
+            if setting is None:
+                setting = PortalSetting(portal_name=portal_name)
+                session.add(setting)
+                # this is needed to get the id of the newly
+                # created setting object.
+                session.flush()
+        return setting

tests/test_models.py
@@ -4,15 +4,48 @@ from unittest import mock
 
 from job_board.models import Job as JobModel
 from job_board.base import Job
+from job_board import config
 
 import sqlalchemy
+import pytest
+
 from job_board.models import (
     store_jobs,
     notify,
 )
+from job_board.connection import get_session
+from job_board.portals.models import PortalSetting
 
 
 now = datetime.now(timezone.utc)
+
+
+def test_session_can_be_used_only_during_tests(db_session):
+    with mock.patch.object(config, "TEST_ENV", False):
+        with pytest.raises(RuntimeError):
+            get_session()
+
+
+def test_read_only_session(db_setup):
+    with get_session(readonly=True) as session:
+        with pytest.raises(sqlalchemy.exc.InternalError) as exc:
+            session.execute(
+                sqlalchemy.insert(JobModel).values(
+                    link="http://example.com",
+                    title="Test Job",
+                    salary=Decimal(str(100_000)),
+                    posted_on=now,
+                )
+            )
+
+    assert "cannot execute INSERT in a read-only transaction" in str(exc.value)
+
+
+def test_portal_setting_get_or_create_with_invalid_portal_name():
+    with pytest.raises(ValueError) as exception:
+        PortalSetting.get_or_create(portal_name="invalid_portal")
+
+    assert "invalid_portal" in str(exception)
 
 
 def test_store_jobs(db_session):
@@ -59,9 +92,6 @@ def test_notify(db_session):
         posted_on=now - timedelta(days=1),
     )
     db_session.add_all([job_1, job_2])
-    db_session.commit()
-
-    from job_board import config
 
     with (
         mock.patch(
@@ -77,7 +107,13 @@ def test_notify(db_session):
     mock_service.assert_called_once()
     assert mock_send_email.call_count == 2
 
-    db_session.refresh(job_1)
-    assert job_1.notified is True
-    db_session.refresh(job_2)
-    assert job_2.notified is True
+    # Check that the jobs are marked as notified
+    ids = [job_1.id, job_2.id]
+    statement = (
+        sqlalchemy.select(sqlalchemy.func.count())
+        .select_from(JobModel)
+        .where(JobModel.id.in_(ids), JobModel.notified.is_(True))
+    )
+    count = db_session.execute(statement).scalar_one()
+
+    assert count == len(ids)

job_board/init_db.py
@@ -1,5 +1,4 @@
 from job_board.models import BaseModel
-from job_board.portals.models import PortalSetting
 from job_board.logger import logger
 from job_board.connection import get_engine
 
@@ -11,4 +10,3 @@ def init_db():
     logger.info("Creating Tables")
     engine = get_engine()
     BaseModel.metadata.create_all(bind=engine)
-    PortalSetting.create_portal_setting()

job_board/logger.py
@@ -3,7 +3,6 @@ import logging
 from job_board import config
 
 log_file = "job-board-test.log" if config.TEST_ENV else "job-board.log"
-
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%z %m/%d/%Y %I:%M:%S %p",

tests/conftest.py
@@ -1,6 +1,5 @@
 import os
 import importlib
-from unittest import mock
 
 import pytest
 from sqlalchemy.orm import sessionmaker
@@ -9,6 +8,8 @@ from job_board import config
 from job_board.init_db import init_db
 from job_board.connection import get_engine
 from job_board.models import BaseModel
+import job_board.connection as connection_module
+from job_board.connection import _test_session
 
 
 def pytest_configure():
@@ -44,7 +45,8 @@ def db_setup():
 @pytest.fixture(scope="function")
 def db_session(db_setup):
     """
-    Returns a sqlalchemy session, and after the test, it tears down everything properly.
+    Returns a sqlalchemy session, and after the test, it tears down
+    everything properly.
     """
     engine = get_engine()
     # ensure the database URL is set correctly
@@ -54,13 +56,15 @@ def db_session(db_setup):
     connection = engine.connect()
     # begin the nested transaction
     transaction = connection.begin()
-    # use a custom session factory for tests.
-    Session = sessionmaker(bind=connection)
-    session = Session()
 
-    with mock.patch("job_board.connection._get_session_factory") as mocked_factory:
-        mocked_factory.return_value = Session
-        yield session
+    TestSession = sessionmaker(bind=connection, future=True)
+    session = TestSession()
+    # This is just a hack to make sure that the session
+    # for the test is same as the one in this fixture.
+    # TODO: find a better way to do this
+    connection_module._test_session = session
+    yield session
+    connection_module._test_session = _test_session
 
     session.close()
     # roll back the broader transaction

commit b00770d497f927006dcab400ca6b97678edb63b4

Fix session usage
- the earlier implementation wasn't committing to the
  database.
  Bug introduced in e14650e8dc618088c76504138a85ef2368520b1e

Also add edited_at and created_at to the base model, which is
helpful in keeping track of change to db rows.

job_board/cli.py
@@ -3,7 +3,6 @@ import subprocess
 import sys
 import time
 import traceback
-from datetime import datetime
 from datetime import timedelta
 from datetime import timezone
 
@@ -17,6 +16,7 @@ from job_board.models import notify
 from job_board.models import store_jobs
 from job_board.portals import PORTALS
 from job_board.portals.models import PortalSetting
+from job_board.utils import utcnow_naive
 
 
 def debugger_hook(exception_type, value, tb):
@@ -120,12 +120,9 @@ def _fetch(
             store_jobs(jobs)
             all_jobs.extend(jobs)
 
-            session = get_session(readonly=False)
-            with session:
+            with get_session(readonly=False) as session:
                 setting = session.get(PortalSetting, setting_id)
-                setting.last_run_at = datetime.now(timezone.utc)
-                session.add(setting)
-                session.flush()
+                setting.last_run_at = utcnow_naive()
 
     if to_notify:
         notify()

job_board/connection.py
@@ -1,5 +1,9 @@
+import contextlib
+from typing import Generator
+
 from sqlalchemy import create_engine
 from sqlalchemy import text
+from sqlalchemy.orm import Session
 from sqlalchemy.orm import sessionmaker
 
 from job_board import config
@@ -26,19 +30,25 @@ def _get_session_factory():
         _SessionFactory = sessionmaker(
             bind=get_engine(),
             future=True,
+            expire_on_commit=False,
         )
     return _SessionFactory
 
 
-def get_session(*, readonly=True):
+@contextlib.contextmanager
+def get_session(*, readonly=True) -> Generator[Session, None, None]:
     global _test_session
     if _test_session:
         if not config.TEST_ENV:
             raise RuntimeError("Please use the test environment to run tests. ")
-        return _test_session
+        yield _test_session
+
+        return None
 
     Session = _get_session_factory()
-    session = Session()
-    if readonly:
-        session.execute(text("SET TRANSACTION READ ONLY"))
-    return session
+    with Session() as session:
+        with session.begin():
+            if readonly:
+                session.execute(text("SET TRANSACTION READ ONLY"))
+
+            yield session

job_board/models.py
@@ -1,6 +1,4 @@
 import itertools
-from datetime import datetime
-from datetime import timezone
 
 from sqlalchemy import Boolean
 from sqlalchemy import Column
@@ -22,23 +20,38 @@ from job_board.connection import get_session
 from job_board.logger import logger
 from job_board.notifier.mail import EmailProvider
 from job_board.utils import jinja_env
+from job_board.utils import utcnow_naive
 
 
 # Base class for all models
 class BaseModel(DeclarativeBase):
-    pass
+    __abstract__ = True
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    created_at = Column(
+        DateTime,
+        default=utcnow_naive,
+        nullable=False,
+        server_default=func.now(),
+    )
+    edited_at = Column(
+        DateTime,
+        default=utcnow_naive,
+        onupdate=utcnow_naive,
+        server_default=func.now(),
+        nullable=False,
+    )
 
 
 class Job(BaseModel):
     __tablename__ = "job"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
     link = Column(String, nullable=False)
     title = Column(String, nullable=False)
     salary = Column(Numeric, nullable=False)
     posted_on = Column(
         DateTime,
-        default=lambda: datetime.now(timezone.utc),
+        default=utcnow_naive,
         nullable=False,
         server_default=func.now(),
     )
@@ -70,8 +83,8 @@ def store_jobs(jobs: JobListing):
         values.append(value)
 
     insert_statement = insert(Job).values(values).returning(Job.id)
-    session = get_session(readonly=False)
-    with session:
+
+    with get_session(readonly=False) as session:
         statement = insert_statement.on_conflict_do_nothing(
             index_elements=[
                 func.lower(Job.link),
@@ -88,8 +101,7 @@ def notify():
         .where(Job.notified.is_(False))
         .order_by(Job.salary.desc(), Job.posted_on.desc())
     )
-    session = get_session(readonly=True)
-    with session:
+    with get_session(readonly=True) as session:
         jobs = session.execute(statement).scalars().all()
         if not jobs:
             logger.info("No new jobs found for notification")
@@ -133,6 +145,6 @@ def notify():
             .where(Job.notified.is_(False), Job.id.in_(batched_ids))
             .values(notified=True)
         )
-        session = get_session(readonly=False)
-        with session:
+
+        with get_session(readonly=False) as session:
             session.execute(statement)

job_board/portals/models.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column
 from sqlalchemy import DateTime
 from sqlalchemy import func
 from sqlalchemy import Index
-from sqlalchemy import Integer
 from sqlalchemy import String
 
 from job_board.connection import get_session
@@ -13,7 +12,6 @@ from job_board.portals.base import PORTALS
 class PortalSetting(BaseModel):
     __tablename__ = "portal_setting"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
     portal_name = Column(String, nullable=False)
     last_run_at = Column(DateTime)
 
@@ -30,8 +28,7 @@ class PortalSetting(BaseModel):
         if portal_name not in PORTALS:
             raise ValueError(f"Portal {portal_name} is not supported")
 
-        session = get_session(readonly=False)
-        with session:
+        with get_session(readonly=False) as session:
             setting = (
                 session.query(PortalSetting)
                 .filter(PortalSetting.portal_name == portal_name)

job_board/utils.py
@@ -20,6 +20,10 @@ from job_board import config
 from job_board.logger import logger
 
 
+def utcnow_naive():
+    return datetime.now(timezone.utc).replace(tzinfo=None)
+
+
 def response_hook(response: httpx.Response) -> None:
     response.raise_for_status()
 

tests/test_models.py
@@ -22,7 +22,8 @@ now = datetime.now(timezone.utc)
 def test_session_can_be_used_only_during_tests(db_session):
     with mock.patch.object(config, "TEST_ENV", False):
         with pytest.raises(RuntimeError):
-            get_session()
+            with get_session():
+                pass
 
 
 def test_read_only_session(db_setup):