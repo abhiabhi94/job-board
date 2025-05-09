c8570a0f72285c2988ff60267e0e696a43e17b37
abhiabhi94 <13880786+abhiabhi94@users.noreply.github.com>
2025-05-09 16:13:05 +0530

    Auto deploy on push

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Exit on any error
+set -euxo pipefail
+
+# Create and activate virtual environment
+python3.13 -m venv venv
+. venv/bin/activate
+
+pip install .
+
+job-board schedule
